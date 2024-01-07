cbd413b507b6c2e3e18345c2c23bedf6e8608741
Robert <130874846+RobFrontend@users.noreply.github.com>
2024-01-07 16:55:52 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import players from "./playersData.js";
-import News from "./api.js";
+import nbaArticles from "./api.js";
 
 function App() {
   return (
@@ -16,6 +16,65 @@ function App() {
     </>
   );
 }
+
+//
+
+const News = () => {
+  const [articlesData, setArticlesData] = useState([]);
+  const [selectedArticleIndex, setSelectedArticleIndex] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await nbaArticles();
+        setArticlesData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSelectArticle = (index) => {
+    setSelectedArticleIndex(index);
+  };
+
+  return (
+    <div className="articles">
+      <div className="article-btn-box">
+        <h3>News:</h3>
+        {articlesData.map((_, i) => (
+          <button
+            className="article-btns"
+            key={i}
+            onClick={() => handleSelectArticle(i)}
+          >
+            #{i + 1}
+          </button>
+        ))}
+      </div>
+      {articlesData.length > 0 && (
+        <div key={selectedArticleIndex} className="article-box">
+          <h2>{articlesData[selectedArticleIndex].title}</h2>
+          <div className="article-source">
+            <p>Source:</p>
+            <p className="source">
+              {articlesData[selectedArticleIndex].source.toUpperCase()}
+            </p>
+            <a
+              href={articlesData[selectedArticleIndex].link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+//
 
 function Header() {
   return <h1>NBA Players Stats</h1>;
